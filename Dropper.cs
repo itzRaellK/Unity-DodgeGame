de83f3c6770b1ddb7a234975b150190b623b53fc
Israel Kessler <israel-kessler@hotmail.com>
2025-08-16 18:22:17 -0300

    Update Dropper.cs

--- Dropper.cs
+++ Dropper.cs
@@ -3,22 +3,31 @@ using UnityEngine;
 
 public class DropObject : MonoBehaviour
 {
-    public float time = 0f;
-    public string timePassed = "";
+    private float time = 0f;
+    //public string timePassed = "";
+    [SerializeField] float timeToWait = 3f;
+    MeshRenderer dropperMeshRenderer; // Jeito correto de declarar variáveis do tipo MeshRenderer
+    Rigidbody dropperRigidbody; // Jeito correto de declarar variáveis do tipo Rigidbody
     void Start()
     {
-        GetComponent<Rigidbody>().useGravity = false;
+        dropperMeshRenderer = GetComponent<MeshRenderer>();
+        dropperRigidbody = GetComponent<Rigidbody>();
+
+        dropperMeshRenderer.enabled = false; // é o mesmo que GetComponent<MeshRenderer>().enabled = false;
+        dropperRigidbody.useGravity = false; // é o mesmo que GetComponent<Rigidbody>().useGravity = false;
+        
     } 
     void Update()
     {
         time = Time.time;
-        timePassed = time.ToString("F0");
-        Debug.Log("Tempo: " + timePassed + " segundos!");
+        //timePassed = time.ToString("F0");
+        //Debug.Log("Tempo: " + time.ToString("F0") + " segundos!");
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (time > timeToWait)
         {
-            GetComponent<Rigidbody>().useGravity = true;
-            Debug.Log("Ativou gravidade!");
+            dropperRigidbody.useGravity = true; // Ativa a gravidade da variável dropperRigidbody
+            dropperMeshRenderer.enabled = true; // Ativa o MeshRenderer da variável dropperMeshRenderer
+            Debug.Log("Ativou a Mesh e Gravity do objeto!");
         }
     }
 }
